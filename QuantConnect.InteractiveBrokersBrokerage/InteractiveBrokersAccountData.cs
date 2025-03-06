c41ec99ffc46ab47a7409844a6b89a44014234ab
Martin-Molinero <martin@quantconnect.com>
2025-03-06 20:36:36 -0300

    Clean up unrequired FA requests (#147)
    * Clean up unrequired FA requests
    
    * Bump IBAutomater to 2.0.84

--- QuantConnect.InteractiveBrokersBrokerage/InteractiveBrokersAccountData.cs
+++ QuantConnect.InteractiveBrokersBrokerage/InteractiveBrokersAccountData.cs
@@ -14,7 +14,6 @@
 */
 
 using System.Collections.Concurrent;
-using QuantConnect.Brokerages.InteractiveBrokers.FinancialAdvisor;
 
 namespace QuantConnect.Brokerages.InteractiveBrokers
 {
@@ -39,11 +38,6 @@ namespace QuantConnect.Brokerages.InteractiveBrokers
         public ConcurrentDictionary<string, Holding> AccountHoldings { get; } = new ConcurrentDictionary<string, Holding>();
 
         /// <summary>
-        /// The configuration data for the financial advisor account
-        /// </summary>
-        public FinancialAdvisorConfiguration FinancialAdvisorConfiguration { get; } = new FinancialAdvisorConfiguration();
-
-        /// <summary>
         /// Clears this instance of <see cref="InteractiveBrokersAccountData"/>
         /// </summary>
         public void Clear()
@@ -51,7 +45,6 @@ namespace QuantConnect.Brokerages.InteractiveBrokers
             AccountProperties.Clear();
             CashBalances.Clear();
             AccountHoldings.Clear();
-            FinancialAdvisorConfiguration.Clear();
         }
     }
 }
