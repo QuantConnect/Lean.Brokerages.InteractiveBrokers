252d0d45bceb29c54487028be413bdbfe5ebc107
Jhonathan Abreu <jdabreu25@gmail.com>
2025-05-15 11:02:29 -0400

    Minor fix

--- QuantConnect.InteractiveBrokersBrokerage/InteractiveBrokersBrokerage.cs
+++ QuantConnect.InteractiveBrokersBrokerage/InteractiveBrokersBrokerage.cs
@@ -1908,7 +1908,7 @@ namespace QuantConnect.Brokerages.InteractiveBrokers
                     {
                         return;
                     }
-                    else if (_algorithm.Settings.IgnoreUnknownAssetTypes)
+                    else if (_algorithm != null && _algorithm.Settings.IgnoreUnknownAssetTypes)
                     {
                         // Let's make it a one time warning, we don't want to flood the logs with this message
                         if (requestInfo?.AssociatedSymbol != null)
